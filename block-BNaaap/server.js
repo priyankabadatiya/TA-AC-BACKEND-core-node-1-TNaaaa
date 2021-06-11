5ac30d4ed3f20f3031c5ba24d06a41e94c50100f
priyabadatiya <priyabadatiya@gmail.com>
2021-06-11 23:48:13 +0530

    Explain with code

---
+++ block-BNaaap/server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req,res){
+    res.writeHead(201, {'Content-type':'text/html'})    
+    res.end('<h1>Welcome</h1>')
+}
+server.listen(4444,()=>{
+    console.log('server listening on port 4444')
+})
